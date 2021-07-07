ab8b7a37fed3db65ce9ffafd4968933e5b689fb5
ecminer-42 <86978375+ecminer-42@users.noreply.github.com>
2021-07-07 10:27:09 -0400

    Update main.py

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ from cell import Cell
 
 # Window setup & global variables
 pygame.init()
-WIDTH, HEIGHT = 800, 800
+WIDTH, HEIGHT = 1200, 800
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption('Evolution')
 background = pygame.Surface(WIN.get_size())
